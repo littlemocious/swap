6ebf0ea76163fe5fe69b8987d7a1c007e1c5c7db
Mo Yao <43544618+littlemocious@users.noreply.github.com>
2018-11-30 15:27:57 -0500

    add shape

--- swap_alg_with_formality.py
+++ swap_alg_with_formality.py
@@ -1,51 +1,28 @@
+from get_swap_data import fetch_data
+from configparser import RawConfigParser
 import pandas as pd
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader, BatchSampler, RandomSampler
+from torch.utils.data import DataLoader
 import torch.functional as F
 import torch.nn.functional as F
 from torch.autograd import Variable
-import matplotlib
-import matplotlib.pyplot as plt
-import pickle
 
 EMBEDDING_DIM = 50
-EPOCH_NUM = 20
+EPOCH_NUM = 30
 BATCH_SIZE = 128
 FORMALITY_SIZE = 10
-#WEIGHT_DECAY = 0
+WEIGHT_DECAY = 0
+SHAPE_SIZE = 10
 
-#formalities: style_idx : style_formality_idx
-#styles: style_name
-#style_to_idx: style_name: style_idx
-#pair_list: (style_name1, style_name2)
-#pair_idx_list: (style_idx1, style_idx2)
+secrets = RawConfigParser()
+secrets.read("secrets.ini")
 
-swap_data = pickle.load(open("swap_data", "rb"))
-formality = pickle.load(open("formality", "rb"))
-
-styles = list(set(list(swap_data['origin_style']) 
-                       + list(swap_data['replace_style'])))
-
-style_to_idx = {style: i for i, style in enumerate(styles)}
-
-pair_list = list(map(tuple,swap_data.values))
-
-formality['style_index'] = formality.apply(lambda x: style_to_idx.get(x['style'], -1), axis = 1)
-formality = formality[formality['style_index'] > -1]
-formalities = formality.set_index('style_index')['formality'].to_dict()
-
-STYLE_SIZE = len(styles)
-
-pair_idx_list = [(style_to_idx[i[0]], style_to_idx[i[1]]) for i in pair_list] + \
-                [(style_to_idx[i[1]], style_to_idx[i[0]]) for i in pair_list]
-    
-with open('styles', 'wb') as f:
-    pickle.dump(styles, f)
-    
-with open('styleToIdx', 'wb') as f:
-    pickle.dump(style_to_idx, f)
-
+def prepare_feature(data, feature_col, style_col = 'style'):
+    data['style_index'] = data.apply(lambda x: style_to_idx.get(x[style_col], -1), axis = 1)
+    data = data[data['style_index'] > -1]
+    data = data.set_index('style_index')[feature_col].to_dict()
+    return(data)
 
 def get_input_layer_batch(word_idx_batch):
     x = torch.zeros([STYLE_SIZE, len(word_idx_batch)]).float()
@@ -53,23 +30,50 @@ def get_input_layer_batch(word_idx_batch):
         x[word_idx_batch[i], i] = 1.0
     return x
 
-def get_input_form_batch(word_idx_batch):
-    y = torch.zeros([FORMALITY_SIZE, len(word_idx_batch)]).float()
+def get_input_feat_batch(word_idx_batch, feat_size, feat_list):
+    y = torch.zeros([feat_size, len(word_idx_batch)]).float()
     for i in range(len(word_idx_batch)):
-        y[formalities[word_idx_batch[i]] - 1, i] = 1.0
+        y[feat_list[word_idx_batch[i]] - 1, i] = 1.0
     return y
 
-weight_decays = [0] + [10 ** i for i in range(-6,0)] + [5 * (10 ** i) for i in range(-6,-1)]
+formality, shape, swap_data = fetch_data(secrets)
+
+swap_data.iloc[swap_data[swap_data['replace_style'] == 'CT34'].index, 1] = 'CT18'
+swap_data.iloc[swap_data[swap_data['origin_style'] == 'CT34'].index, 0] = 'CT18'
+
+styles = list(set(list(swap_data['origin_style']) + list(swap_data['replace_style'])))
+STYLE_SIZE = len(styles)
+
+style_to_idx = {style: i for i, style in enumerate(styles)}
+
+pair_list = list(map(tuple,swap_data.values))
+        
+for i in pair_list:
+    if i[0] == i[1]:
+        pair_list.remove(i)
+
+pair_idx_list = [(style_to_idx[i[0]], style_to_idx[i[1]]) for i in pair_list] + \
+                [(style_to_idx[i[1]], style_to_idx[i[0]]) for i in pair_list]
+    
+    
+shape_to_idx = {shape: i for i, shape in enumerate(set(shapes.values()))}
+shape['shape_idx'] = shape.apply(lambda x: shape_to_idx.get(x['shape'], -1), axis = 1)
+shapes = prepare_feature(shape,'shape_idx')
+
+formalities = prepare_feature(formality, 'formality')
+weight_decays = [0, 1e-5, 5e-5, 1e-4]
 
 for j in range(len(weight_decays)):
     losses = []
     W1 = Variable(torch.randn([EMBEDDING_DIM, STYLE_SIZE]).float(), requires_grad=True)
     W2 = Variable(torch.randn([EMBEDDING_DIM, FORMALITY_SIZE]).float(), requires_grad=True)
-    optimizer = torch.optim.Adam([W1, W2], lr = 1e-3, weight_decay = weight_decays[j])
-    print('weight is' + str(weight_decays[j]))
+    W3 = Variable(torch.randn([EMBEDDING_DIM, SHAPE_SIZE]).float(), requires_grad=True)
+    optimizer = torch.optim.Adam([W1, W2, W3], lr = 1e-3, weight_decay = weight_decays[j])
+    print('weight is ' + str(weight_decays[j]))
     loss_function = torch.nn.NLLLoss()
-    form_mat = get_input_form_batch(list(range(STYLE_SIZE)))
-
+    form_mat = get_input_feat_batch(list(range(STYLE_SIZE)), FORMALITY_SIZE, formalities)
+    shape_mat = get_input_feat_batch(list(range(STYLE_SIZE)), SHAPE_SIZE, shapes)
+    
     for epoch in range(EPOCH_NUM):
         print('-----------------------' + str(epoch) + '-----------------------')
         data_loader = DataLoader(pair_idx_list, batch_size = BATCH_SIZE, shuffle = True)
@@ -77,11 +81,12 @@ for j in range(len(weight_decays)):
         i = 0
         for item1s, item2s in data_loader:
             item1s_idxs = get_input_layer_batch(item1s)
-            item1s_form = get_input_form_batch(item1s.numpy())
+            item1s_form = get_input_feat_batch(item1s.numpy(), FORMALITY_SIZE, formalities)
+            item1s_shape = get_input_feat_batch(item1s.numpy(), SHAPE_SIZE, shapes)
             item2s_idxs = torch.tensor(item2s, dtype = torch.long)
             optimizer.zero_grad()
-            z1 = torch.matmul(W1, item1s_idxs) + torch.matmul(W2, item1s_form)
-            z2 = W1 + torch.matmul(W2, form_mat)
+            z1 = torch.matmul(W1, item1s_idxs) + torch.matmul(W2, item1s_form) + torch.matmul(W3, item1s_shape)
+            z2 = W1 + torch.matmul(W2, form_mat) + torch.matmul(W3, shape_mat)
             z3 = torch.matmul(z2.t(), z1)
             log_probs = torch.nn.functional.log_softmax(z3, dim=0).t()
             loss = loss_function(log_probs, item2s_idxs)
@@ -95,5 +100,4 @@ for j in range(len(weight_decays)):
         print(f'Loss at epoch {epoch}: {total_loss}')
         torch.save(W1,'w1' + str(j) + '.pt')
         torch.save(W2,'w2' + str(j) + '.pt')
-    with open('loss' + str(j), 'wb') as f:
-    	pickle.dump(losses, f)
+        torch.save(W3,'w3' + str(j) + '.pt')
